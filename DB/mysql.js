752e744c34773868d50c1573e757e064924ae850
Fidel_Nava <eduardonaru@hotmail.com>
2023-10-26 12:27:43 -0600

    Haciendo el documento de conexion

--- DB/mysql.js
+++ DB/mysql.js
@@ -1,10 +1,15 @@
-import mysql from 'serverless-mysql';
-const db = mysql({
-  config: {
-    host: 'localhost',
-    port: 3306,
-    database: 'biblioteca',
-    user: 'root',
-    password: ''
+const { createPool } = require("mysql");
+const pool = createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  port: 3306,
+  database: "biblioteca",
+});
+
+pool.getConnection((err) => {
+  if(err){
+    console.log("Error al conectar con BD");
   }
+  console.log("Conectado a BD");
 });
