43be7db6db75f861bc3f2bcf77c2a15a5d6350bf
Eduardoespl <eduardoesp3317@gmail.com>
2023-11-15 14:22:43 -0600

    Finished components and screens

--- src/pages/components/makePrestamoButton.js
+++ src/pages/components/makePrestamoButton.js
@@ -1,22 +1,34 @@
+// makePrestamoButton.js
 import React from "react";
 
-export default function MakePrestamoButton({ selectedUser }) {
-    function makePrestamo() {
-        if (selectedUser) {
-            const userName = selectedUser.nombre;
-            alert(`Prestamo Realizado para ${userName}`);
-        }
-    }
+const MakePrestamoButton = ({ selectedUser, selectedLibro }) => {
+    const handleClick = () => {
+        // Acceder al valor del usuario seleccionado y del libro seleccionado directamente desde las props
+        console.log("Usuario seleccionado:", selectedUser);
+        console.log("Libro seleccionado:", selectedLibro);
+
+        // Puedes realizar otras acciones con los valores seleccionados aquí
+    };
+
+    const isButtonDisabled = !selectedUser || !selectedLibro;
 
     return (
         <>
+            {selectedUser && (
+                <p>Usuario seleccionado: {selectedUser.nombre}</p>
+            )}
+            {selectedLibro && (
+                <p>Libro seleccionado: {selectedLibro.titulo}</p>
+            )}
             <button
-                onClick={makePrestamo}
-                className="bg-white text-black font-bold py-2 px-4 rounded-lg"
-                disabled={!selectedUser}
+                onClick={handleClick}
+                className={`py-2 px-4 rounded cursor-pointer mt-4 ${isButtonDisabled ? "bg-gray-300 cursor-not-allowed" : "bg-green-500 hover:bg-green-700 text-white"}`}
+                disabled={isButtonDisabled}
             >
-                Prestamo
+                Make Prestamo
             </button>
         </>
     );
-}
+};
+
+export default MakePrestamoButton;
